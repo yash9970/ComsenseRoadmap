56e49cce5b335b6f231760e7aa7e372e0d49c4bd
yash9970 <82251298+yash9970@users.noreply.github.com>
2025-01-08 14:38:35 +0530

    Add files via upload
    example of while loop

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+public class Whileloop
+{
+	public static void main(String[] args) 
+	{
+	    int n=2;
+	    int i=1;
+	    while(i<11)
+	    {
+	        System.out.println(n*i);
+	        i++;
+	    }
+	}
+}
