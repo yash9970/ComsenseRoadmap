5d5c305ed7d23e57307e99e3ad6ae2d0d5c1f843
yash9970 <82251298+yash9970@users.noreply.github.com>
2025-01-08 12:02:43 +0530

    Add files via upload
    Primitive datatypes in java

---
+++ Datatypes.java
@@ -0,0 +1,34 @@
+public class Datatypes
+{
+	public static void main(String args[])
+	{
+		/*we are two types of datatype  
+		1.Primitive Data types 
+		2.Non-Primitive Data types
+		*/
+	//lets see primitive first- Primitive are predefine datatypes in java
+		int a=10;
+		System.out.println(a);
+
+		
+		char b='y';
+  		System.out.println(b);
+
+		boolean c=true;
+		System.out.println(c);
+		double d=7.2;
+		System.out.println(d);
+
+		float e=12.300000f;
+		System.out.println(e);
+
+		long f=12;
+		System.out.println(f);
+
+		short g=2;
+		System.out.println(g);
+
+		byte h=12;
+		System.out.println(h);
+	}
+}
